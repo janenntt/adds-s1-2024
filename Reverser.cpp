2f587e661ea7377d0ef31290fd799d4e7472ac9b
jane <tramnguyennt1707@gmail.com>
2024-03-12 17:29:22 +1030

    add

--- Reverser.cpp
+++ Reverser.cpp
@@ -1,17 +1,27 @@
 #include "Reverser.h"
-int Reverser::x = 0; 
+#include <math.h>
+// int Reverser::reverseDigit(int value){
+//     static int x = 0 ;
+//     if (value < 0){
+//         return -1;
+//     } else if (value == 0){
+//         return x;
+//     } else {
+//         x = x*10 + (value % 10);
+//         return reverseDigit(value / 10);
+//     }ß
+// }
+int Reverser::height = 0;
 int Reverser::reverseDigit(int value){
-
-    if (value < 0){
-        return -1;
-    } else if (value == 0){
-        return x;
-    } else {
-        x = x*10 + (value % 10);
-        return reverseDigit(value / 10);
-    }
-}
-
+    
+     if (value < 0 ) {
+         return -1;
+     }
+     if (value == 0) {
+         return 0;
+     }
+     return reverseDigit(value/10)+(value%10)*pow(10,height++);
+ }
 string Reverser::reverseString(string characters){
     static int index = characters.length()-1;
     static string new_string = "";
--- main.cpp
+++ main.cpp
@@ -2,10 +2,10 @@
 #include "Reverser.h"
 #include <iostream>
 using namespace std;
-
 int main() {
     cout << Truckloads::numTrucks(10, 2) << endl; // Assuming numTrucks is declared in Truckloads.h
-    cout << Reverser::reverseDigit(12345) << endl;
+    cout << Reverser::reverseDigit(1005) << endl;
+    cout << Reverser::reverseDigit(10051) << endl;
     cout << Reverser::reverseString("happy") << endl;
     return 0;
 }
